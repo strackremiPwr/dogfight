aea2dc7b357e644470b1d089c08b40bebc8c49a0
lenovo ThinkBook <lenovo ThinkBook@P-W-Rmi>
2022-10-12 15:10:40 +0100

    Phase 2

---
+++ dogfight/src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder {
+	
+	private int player;
+	
+	public UserOrder() {
+		
+	}
+	
+	public int getOrder() {
+		return 1;
+	}
+	
+	public int getPlayer() {
+		return player;
+	}
+	
+	
+}
